058931a845e7bc24288644e1c4cb8f168a7ee08a
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-03-12 10:07:32 -0500

    Add search states in index.js file

---
+++ src/components/Search/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Container from "./components/Container";
+import SearchForm from "./components/SearchForm";
+import SearchResults from "./components/SearchResults";
+
+class Search extends Component {
+    state = {
+        search: "",
+        employees: [],
+        filtered: [],
+        results: [],
+        error: ""
+
+    };
+
+
+    componentDidMount() {
+        API.getRandomUsers()
+        .then(res => this.setState({ employees: res.data }))
+        .catch(err => console.log(err));
+        console.log(this.state.employees);
+    }
+
+    handleInputChange = event => {
+        this.setState({ search: event.target.value });
+    }
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        API.getRandomUsers(this.state.search)
+        .then(res => {
+            if (res.data.status === "error") {
+                throw new Error(res.data);
+            }
+            this.setState({ results: res.data, error: "Search Empolyee Name"});
+        }).catch(err => this.setState({ error: err}));
+    };
+
+    render() {
+        return (
+            <div>
+                <Container style={{ minHeight: "75% "}}/>
+                <h1 className="text">Employee Directory Search</h1>
+                <SearchForm
+                    handleFormSubmit={this.handleFormSubmit}
+                    handleInputChange={this.handleInputChange}
+                    employees={this.state.employees}/>
+                <SearchResults results={this.state.results}/>
+                <Container/>
+            </div>
+        )
+    }
+}
+
+export default Search;
+
