c5af7a3f869269d67718abb8dc7ea4b35b25c000
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-03-25 19:04:00 -0400

    Add resolved button to sort for Austin;-)

--- src/components/ExploreEmployees/index.js
+++ src/components/ExploreEmployees/index.js
@@ -4,8 +4,8 @@ import React from "react";
 export default function ExploreEmployees(props) {
     return (
         <div>
-            <button onClick={() => props.handleSortEmployeeByState()} className="searchBtn">Search By State</button>
-            <button onClick={() => props.handleSortEmployeeByGender()} className="searchBtn">Search By Gender</button>
+            <button onClick={() => props.handleSortEmployeeByState()} className="searchBtn">Sort By State</button>
+            <button onClick={() => props.handleSortEmployeeByGender()} className="searchBtn">Sort By Gender</button>
             
         </div>
     )
